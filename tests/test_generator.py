607a9de9f98b982d0c221dbdc6f49fd31956037e
Zhan Rongrui <2742392377@qq.com>
2023-12-01 15:10:05 +0800

    ✅ test: add generator test

---
+++ tests/test_generator.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from sys import stdout
+from typing import ParamSpec, TypeAlias, TypeVar, TypeVarTuple
+
+import typer
+from loguru import logger
+
+from auto_config.device import Device
+from auto_config.generator import (
+    AnsibleHostsGenerator,
+    CaddyFileGenerator,
+    DNSConfigGenerator,
+    GeneratorBase,
+    SSHHostsGenerator,
+)
+
+
+def test_generator_base():
+    generator = GeneratorBase()
+    generator.add_line("line1")
+    generator.add_block(start_line="start1", end_line="end1")
+    generator.add_line("line2")
+    assert generator.code == "\nline1\n\nstart1\nline2\nend1"
+    generator.clear()
+    assert generator._generated_code is None
+    assert generator.code == "\nline1\n\nstart1\nline2\nend1"
+    # generator.write("path")
+
+
+def test_generator_init():
+    devices = []
+    AnsibleHostsGenerator(devices)
+    SSHHostsGenerator(devices)
+    CaddyFileGenerator(devices)
+    DNSConfigGenerator(devices)
