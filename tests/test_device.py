94ef4231a75d866dec8eda04d0cff91834762c10
zrr1999 <2742392377@qq.com>
2024-04-10 23:09:14 +0800

    🐛 fix: update pre-commit and fix tests

--- tests/test_device.py
+++ tests/test_device.py
@@ -4,10 +4,10 @@ from auto_config.device import Device
 
 
 def test_get_name():
-    device = Device(system="system1", hardware="hardware1", group="group1", main=True, desc="")
+    device = Device(system="system1", hardware="hardware1", group="group1", desc="")
     assert device.get_name() == "group1-hardware1-system1"
 
 
 def test_get_domain():
-    device = Device(system="system1", hardware="hardware1", group="group1", main=True, desc="")
+    device = Device(system="system1", hardware="hardware1", group="group1", desc="")
     assert device.get_domain() == "hardware1-system1.ssh.group1"
